4bb49870a3d89202fdd51aacb9cdab8beb64da58
Sergey <aliger99@rambler.ru>
2022-09-25 15:04:54 +0700

    add d_001

---
+++ d_002.py
@@ -0,0 +1,9 @@
+# Пользователь вводит время в секундах. Переведите время в часы, минуты, секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
+
+import datetime
+sec = 3661
+minute = sec % 60
+sec1 = minute % 60
+hour = int(sec/3600)  # 1 hour
+timeobj = datetime.time(hour, minute, sec1)
+print(timeobj)
