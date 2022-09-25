d01af681c07273fa795a1962e6b54dd0becc6ca9
Sergey <aliger99@rambler.ru>
2022-09-25 14:37:08 +0700

    add 011_1

--- 011.py
+++ 011.py
@@ -1,15 +1,7 @@
 # 11.	Сформировать список из  N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 N = int(input('Введите N '))
-# print('Введите N ');
-# N = int(input())
-# from random import randint
-# l = [randint(-99,99) for x in range(N)]
-# print(l)
-# matrix = []
-# for i in range(n):
-#     matrix.append((-3)**i)
-# print(f"Итоговая последовательность: {matrix}")
+
 start = 1
 for i in range(1, N+1):
     print(start, end=' ')
