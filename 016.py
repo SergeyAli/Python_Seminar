62ae2920f3343bcf1f48ab80a294494f0424506d
Sergey <aliger99@rambler.ru>
2022-09-30 18:06:46 +0700

    add 016

--- 016.py
+++ 016.py
@@ -1,21 +1,11 @@
 # Задать список из n чисел последовательности(1 +〖1 / n)〗 ^ n и вывести на экран их сумму
 
-# print('Введите N ');
-# N = int(input())
-# umbers = list(range(1, N+1))
-# for i in umbers:
-#     print(f"{umbers[i-1]} : {3*i+1}", end = ", ")
-
-# n = 5
-# lst = [i] * (1+(1/n))**n
-# print(lst)
-
 
 print('Введите N ');
-N = int(input())
-sum_N = list(range(1, N+1))
-f=0
-for i in sum_N:
-    f += (1 + (1 / i)) ** i
-    print(f"{f}",  end = ", ")
+n = int(input())
+N = list(range(1, n+1))
+sum_N=0
+for i in N:
+    sum_N += (1 + (1 / i)) ** i
+    print(f"{sum_N}",  end = ", ")
 
