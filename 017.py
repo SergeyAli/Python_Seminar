1d418e54db60d51b9f852d34adca499ec33f2458
Sergey <aliger99@rambler.ru>
2022-10-01 15:15:01 +0700

    add 017

--- 017.py
+++ 017.py
@@ -1,10 +1,24 @@
-# 17.	Задать список из N элементов, заполненных числами из [-N, N].
-# Найти произведение элементов на указанных позициях.
-# Позиции хранятся в файле file.txt в одной строке одно число
+# 17.	# Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях.
+# # Позиции хранятся в файле file.txt в одной строке одно число
+#
+n = 8
+numbers = []
+for i in range(-n, n + 1):
+    numbers.append(i)
+print(numbers)
 
-N = int (input (print ('Введите целое число, последовательность которых будеть выводиться с отрицательного до положительного диапазона')))
-a = N - N*2;
-while(a<=N):
-    print(f"{a} ")
-    a+=1
+with open('file.txt', 'w') as data:
+    data.write("1\n")
+    data.write("3\n")
+    data.write("hdjfh")
 
+path = 'file.txt'
+data = open(path, 'r')
+total = 1
+for line in data:
+    if line.isdigit():
+        if int(line) <= len(numbers):
+            total *= numbers[int(line)]
+data.close()
+
+print(total)
