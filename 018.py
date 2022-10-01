c7a1eba622af50863b9a3747ea91f4ce8418dfe8
Sergey <aliger99@rambler.ru>
2022-10-01 15:35:08 +0700

    add 018

--- 018.py
+++ 018.py
@@ -1,4 +1,10 @@
-# 18.	Реализовать алгоритм перемешивания списка
+# 18.	# Реализовать алгоритм перемешивания списка.
+number = [1, 2, 8, 6, 5, 7, 12, 55]
+print(number)
+for i in range(0, int(len(number)//2)):
+    number[i], number[len(number)-i - 1] = number[len(number)-i - 1], number[i]
+
+print(number)
 
 
 
