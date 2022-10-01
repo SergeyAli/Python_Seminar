7679d43a033796c5238d6a2735f5d163e3ce46da
Sergey <aliger99@rambler.ru>
2022-10-01 14:14:24 +0700

    add 007_1

--- 007.py
+++ 007.py
@@ -20,3 +20,13 @@ if checkPredicate(statement) == True:
     print(f"Утверждение истинно")
 else:
     print(f"Утверждение ложно")
+
+# Решения преподавателя, значение переменных или 0 или 1
+
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             if not (x or y or z) == (not x and not y and not z):
+#                 print(f'For {x}, {y}, {z}:', True)
+#             else:
+#                 print(False)
