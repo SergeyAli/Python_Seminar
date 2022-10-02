a7ad5ff6cfcafe5f2c2491ae120887e02950c8d3
Sergey <aliger99@rambler.ru>
2022-10-02 15:09:33 +0700

    add 029

--- 029.py
+++ 029.py
@@ -1,1 +1,12 @@
 # 29.	Найти НОК двух чисел
+n = 2
+n1 = 5
+if n > n1:
+    maximum = n
+else:
+    maximum = n1
+
+if maximum % n == 0 and maximum % n1 == 0:
+    print(f"НОК = {n1}")
+else:
+    print(f"НОК = {n1 * n}")
