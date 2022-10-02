fff3ff523cd2fcf1262b1c20d9c3cbdc494b547e
Sergey <aliger99@rambler.ru>
2022-10-02 14:56:16 +0700

    add 028

--- 028.py
+++ 028.py
@@ -1,3 +1,21 @@
 # 28.	Найти корни квадратного уравнения Ax² + Bx + C = 0
 # a.	Математикой
 # b.	Используя дополнительные библиотеки*
+
+from math import *
+a = float(input("a "))
+b = float(input("b "))
+c = float(input("c "))
+x = 0
+discriminant = (b ** 2) - (4 * a * c)
+
+if discriminant < 0:
+    print("Нет корней")
+elif discriminant == 0:
+    x = -b / (2 * a)
+    print(x)
+elif discriminant > 0:
+    x1 = (-b - sqrt(discriminant)) / (2 * a)
+    x = (-b + sqrt(discriminant)) / (2 * a)
+    print(x1)
+    print(x)
