5105aa3eec4ee2a9342e831ad18a74e1ef60fb5b
Sergey <aliger99@rambler.ru>
2022-10-02 16:07:24 +0700

    add 012_1

--- 012.py
+++ 012.py
@@ -1,9 +1,24 @@
 # Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-print('Введите N ');
-N = int(input())
-umbers = list(range(1, N+1))
-for i in umbers:
-    print(f"{umbers[i-1]} : {3*i+1}", end = ", ")
+# 1 Вариант
+# print('Введите N ');
+# N = int(input())
+# umbers = list(range(1, N+1))
+# for i in umbers:
+#     print(f"{umbers[i-1]} : {3*i+1}", end = ", ")
+#
+# # 2 Вариант
+# n = int(input('N = '))
+# d = {}
+# for i in range(1, n+1):
+#     d.update({i: 3*i + 1})
+# print(d)
 
+# 3 Вариант
+
+n = int(input('N = '))
+d = {}
+for i in range(1, n+1):
+    d.setdefault(i, 3*i + 1)
+print(d)
