be6318f9514b92892a27e76f2eca31d7b0b5165d
Sergey <aliger99@rambler.ru>
2022-10-02 13:35:10 +0700

    add 022

--- 018.py
+++ 018.py
@@ -9,31 +9,31 @@ print(number)
 
 # Еше одно решение
 ## Реализовать алгоритм перемешивания списка.
-original = [0, 2, 4, 6, 9]
-print(original)
-
-import time
-
-
-from time import time
-
-def time_random():
-    print(time() - float(str(time()).split('.')[0]))
-    return time() - float(str(time()).split('.')[0])
-
-
-def random_number(min, max):
- return int(time_random() * (max - min) + min)
-
-def mixing_list(array):
-    for i in range(0, len(array)):
-        rnd_position = int(random_number(0, len(array)))
-        temp1 = original[i]
-        original[i] = original[rnd_position]
-        original[rnd_position] = temp1
-
-mixing_list(original)
-print(original)
+# original = [0, 2, 4, 6, 9]
+# print(original)
+#
+# import time
+#
+#
+# from time import time
+#
+# def time_random():
+#     print(time() - float(str(time()).split('.')[0]))
+#     return time() - float(str(time()).split('.')[0])
+#
+#
+# def random_number(min, max):
+#  return int(time_random() * (max - min) + min)
+#
+# def mixing_list(array):
+#     for i in range(0, len(array)):
+#         rnd_position = int(random_number(0, len(array)))
+#         temp1 = original[i]
+#         original[i] = original[rnd_position]
+#         original[rnd_position] = temp1
+#
+# mixing_list(original)
+# print(original)
 
 
 
