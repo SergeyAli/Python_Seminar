74229c8217ccbd8d29a1d69b3168a2b105954f83
Sergey <aliger99@rambler.ru>
2022-10-06 20:09:39 +0700

    add 021

--- 021.py
+++ 021.py
@@ -6,9 +6,14 @@
 # список: ["123", "234", 123, "567"], ищем: "123", ответ: -1
 # список: [], ищем: "123", ответ: -1
 
-# arr=["qwe", "asd", "zxc", "qwe", "ertqwe"]
-# b=["qwe"]
-# print(arr, b)
-# if b[0] in arr:
-#     del arr[arr.index(b[0])]  
-# print(arr.index(b[0])+1)
+arr=["123", "234", 123, "567"]
+b=["123"]
+print(arr, b)
+
+if b[0] in arr:
+    del arr[arr.index(b[0])]
+try:
+    index = arr.index(b[0])
+except ValueError:
+    index = -2
+print(index+1)
