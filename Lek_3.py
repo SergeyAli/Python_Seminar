2e5a2890e6b8e5bc56e47c268022296105c4f4ac
SergeyAli <108223744+SergeyAli@users.noreply.github.com>
2022-10-07 14:04:34 +0700

    Update Lek_3.py

--- Lek_3.py
+++ Lek_3.py
@@ -162,3 +162,28 @@ data = [x for x in range(10)]
 
 res = list(filter(lambda x: x%2==0, data))
 print(res)
+
+# data = '1 2 3 5 8 15 23 38'.split()
+
+# res = map(int, data)
+# res = filter(lambda x: not x%2, res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+
+# Функция ZIP
+
+
+# user = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
+# data = list(zip(user, ids, salary))
+# print(data)
+
+# Функция enumeratte -  если требуеться пронумеровать свой список нужно взть два
+
+user = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+salary = [111, 222, 333]
+data = list(enumerate(user))
+print(data)
