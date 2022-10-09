cf4d18ffccfc12fd9f751f8a2062210089c418a6
Sergey <aliger99@rambler.ru>
2022-10-09 15:10:44 +0700

    add 041

--- 041.py
+++ 041.py
@@ -2,4 +2,30 @@
 # Используются операции +,-,/,*. приоритет операций стандартный. Пример: 2+2 => 4; 1+2*3 => 7; 1-2*3 => -5;
 # a.	Добавить возможность использования скобок, меняющих приоритет операций. Пример: 1+2*3 => 7; (1+2)*3 => 9;
 
+import re
 
+actions = {
+    "^": lambda x, y: str(float(x) ** float(y)),
+    "*": lambda x, y: str(float(x) * float(y)),
+    "/": lambda x, y: str(float(x) / float(y)),
+    "+": lambda x, y: str(float(x) + float(y)),
+    "-": lambda x, y: str(float(x) - float(y))
+}
+
+priority_reg_exp = r"\((.+?)\)"
+action_reg_exp = r"(-?\d+(?:\.\d+)?)\s*\{}\s*(-?\d+(?:\.\d+)?)"
+
+
+def my_eval(expresion: str) -> str:
+    while (match := re.search(priority_reg_exp, expresion)):
+        expresion: str = expresion.replace(match.group(0), my_eval(match.group(1)))
+
+    for symbol, action in actions.items():
+        while (match := re.search(action_reg_exp.format(symbol), expresion)):
+            expresion: str = expresion.replace(match.group(0), action(*match.groups()))
+
+    return expresion
+
+exp = input()
+# exp = "(1 + 4) * (5 * (10 - 2)) / 5"
+print(my_eval(exp), eval(exp))  # 40.0 40.0
