bdf56577911768bfa836313e4a6dd0a7aafc60c6
Sergey <aliger99@rambler.ru>
2022-10-13 22:21:16 +0700

    add Lek_4

--- Lek_4/001/html_creater.py
+++ Lek_4/001/html_creater.py
@@ -17,3 +17,22 @@ def create(device = 1):
         page.write(html)
 
     return html
+
+
+
+def new_create(data, device = 1):
+    t, p, w = data
+    style = 'style ="font-size:22px"'
+    html = '<html>\n <head></head>\n <body>\n'
+    html += '    <p {}>Temperature: {} c</p>\n' \
+         .format(style, t)
+    html += '    <p {}> Wind_speed: {} m/c</p>\n' \
+        .format(style,  w)
+    html += '    <p {}> Pressure: {} mmHg/c</p>\n' \
+        .format(style, p)
+    html += '   </body>\n,html>'
+
+    with open('new_index.html', 'w') as page:
+        page.write(html)
+
+    return data
