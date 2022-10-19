5330223d0dbb7898618d767754af47c51872627b
Sergey <aliger99@rambler.ru>
2022-10-19 21:07:56 +0700

    add Sem

--- Sem/Sem_001.py
+++ Sem/Sem_001.py
@@ -2,6 +2,14 @@ import pandas as pd
 # Load the xlsx file
 excel_data = pd.read_excel(r'E:\GeekBrains\08\python\Seminar\Sem\Sem_001.xlsx')
 # Read the values of the file in the dataframe
-data = pd.DataFrame(excel_data, columns=['Sales Date', 'Sales Person', 'Amount'])
+data = pd.DataFrame(excel_data, columns=['UID', 'phone:', 'surname:', 'name:', 'patronymic:'])
 # Print the content
 print("The content of the file is:\n", data)
+
+# row = data.iloc[1] #index=1 => second row Получить определенную строку, используя индекс
+# print(row)
+
+index = data.index
+
+for i in index:
+    print(i)
---
+++ Sem_7_D/12345.py
@@ -0,0 +1,5 @@
+with open(r'Sem_7_D/uses.txt', 'rb') as f:
+    print(f.read().decode('Windows 1251'))
+
+# with open('str_en_text_1.txt', 'rb') as f:
+#     print(f.read().decode('utf-8'))
