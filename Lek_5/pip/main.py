ac06e3cc968a149862557723ef8ed416ae9af7f6
Sergey <aliger99@rambler.ru>
2022-10-23 12:35:21 +0700

    Lek_5

--- Lek_5/pip/main.py
+++ Lek_5/pip/main.py
@@ -35,26 +35,26 @@
 
 # import matplotlib.pyplot as plt
 # import numpy as np
-
+#
 # # Fixing random state for reproducibility
 # np.random.seed(19680801)
-
-
+#
+#
 # plt.rcdefaults()
 # fig, ax = plt.subplots()
-
+#
 # # Example data
 # people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
 # y_pos = np.arange(len(people))
 # performance = 3 + 10 * np.random.rand(len(people))
 # error = np.random.rand(len(people))
-
+#
 # ax.barh(y_pos, performance, xerr=error, align='center')
 # ax.set_yticks(y_pos, labels=people)
 # ax.invert_yaxis()  # labels read top-to-bottom
 # ax.set_xlabel('Performance')
 # ax.set_title('How fast do you want to go today?')
-
+#
 # plt.show()
 
 # Телеграмм бот oh_my_pythen_bot_99 (oh_my_pythen_bot_99_bot)
@@ -67,9 +67,9 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("5713892468:AAGR6Dhsmo6q3DToNEpFYXCePd5UTXx1iBw").build()
+app = ApplicationBuilder().token('5713892468:AAGR6Dhsmo6q3DToNEpFYXCePd5UTXx1iBw').build()
 
 app.add_handler(CommandHandler("hello", hello))
 
 app.run_polling()
-
+#
--- Lek_5/telegram/main.py
+++ Lek_5/telegram/main.py
@@ -3,7 +3,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
 
-app = ApplicationBuilder().token("5738163783:AAFU7gKJ4w3eb3_W8v7_WT03jUCVbtLsQYk").build()
+# app = ApplicationBuilder().token("5738163783:AAFU7gKJ4w3eb3_W8v7_WT03jUCVbtLsQYk").build()
+app = ApplicationBuilder().token("5713892468:AAGR6Dhsmo6q3DToNEpFYXCePd5UTXx1iBw").build()
 print("start")
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
@@ -11,3 +12,5 @@ app.add_handler(CommandHandler("help", help_command))
 app.add_handler(CommandHandler("sum", sum_command))
 
 app.run_polling()
+
+
